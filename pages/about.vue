4eab74ccf8ef9c16b154e3ec23069ff2ecf3ac92
mohxt <ousta2>
2022-05-21 11:14:34 +0400

    nuxtjs_tailwind_v1

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="flex justify-center bg-green-500 text-5xl">About</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+    <nuxt-link to='/about'>about</nuxt-link>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask" />
+      <button @click="addTask">Addd</button>
+    </div>
+
+    <div class="tasks">
+      <Task
+        v-for="t in $store.state.tasks"
+        :key="t.content"
+        :task="t" />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
